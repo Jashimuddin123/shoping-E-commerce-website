8bcac6314fbd554d7f8921f77fd8119a460d1483
Jashimuddin123 <adnanmahmud3394@gmail.com>
2025-01-14 16:58:05 +0600

    feaching data for showing al proiducts

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -259,15 +259,15 @@ const Footer = () => {
             <div className="mt-4 flex items-center gap-2">
          
             {/* facebook here */}
-            {/* <div>
+            <div>
       <a
         href="https://www.facebook.com/mdjashimuddin.uddin.79" // Replace with your Facebook profile URL
         target="_blank"
         rel="noopener noreferrer"
       >
-        <FaFacebook size={24} className="text-blue-600 hover:text-blue-800" />
+        <FaFacebook size={24} className="text-blue-500 hover:text-blue-800" />
       </a>
-    </div> */}
+    </div>
               {/* instagrams  here*/}
               <div>
       <a
